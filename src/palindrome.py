179c1e052317b87f856d187a3a1d549479e129ec
facu <f.affar.com>
2025-04-15 16:13:28 -0300

    implemetacion de funcion y comparacion de caracteres

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,5 +1,17 @@
 import re
 
-def is_palindrome(texto):#definimos is_palindrome 
+def is_palindrome(texto):  # definimos is_palindrome 
     texto_limpio = re.sub(r'[^a-zA-ZáéíóúÁÉÍÓÚñÑ]', '', texto).lower()
-    return texto_limpio == texto_limpio[::-1] 
+    return texto_limpio == texto_limpio[::-1]
+
+
+if __name__ == '__main__':
+    try:
+        while True:
+            entrada = input("Ingrese una palabra o frase: ")
+            if is_palindrome(entrada):
+                print(f'"{entrada}" es un palíndromo\n')
+            else:
+                print(f'"{entrada}" no es un palíndromo\n')
+    except KeyboardInterrupt:
+        print("\nPrograma terminado.")
