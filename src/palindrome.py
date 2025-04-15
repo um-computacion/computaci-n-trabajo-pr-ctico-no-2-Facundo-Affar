a6331b6f35302b6d6a2788fda314478eaf693124
facu <f.affar.com>
2025-04-15 15:07:52 -0300

    Implementación de la comparacion de caracteres

--- src/palindrome.py
+++ src/palindrome.py
@@ -2,4 +2,4 @@ import re
 
 def is_palindrome(texto):#definimos is_palindrome 
     texto_limpio = re.sub(r'[^a-zA-ZáéíóúÁÉÍÓÚñÑ]', '', texto).lower()
-    return False
+    return texto_limpio == texto_limpio[::-1] 
